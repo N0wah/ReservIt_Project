4f83d99869c9263a9ce0193ba6dafc67e2164e23
N0wah <noah68mollet@gmail.com>
2025-06-11 23:07:08 +0200

    Update urls.py

--- backend/reservit/urls.py
+++ backend/reservit/urls.py
@@ -20,5 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('api.urls')),  # Redirect root URL to API
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
 ]
