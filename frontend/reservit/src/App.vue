76393456ee4dd831fe8894f60e5c4a3d4a2aaaa2
N0wah <noah68mollet@gmail.com>
2025-06-12 10:22:46 +0200

    Update App.vue

--- frontend/reservit/src/App.vue
+++ frontend/reservit/src/App.vue
@@ -1,48 +1,22 @@
 <template>
   <div class="flex w-full justify-center bg-black">
-  <div class="max-w-[512px] w-full">
-    <router-view />
-  
-
-  <!-- Affiche la Navbar pour toutes les routes sauf celles exclues, si l'user n'est pas restaurant et pas admin, si on n'est pas sur BookingPage et pas sur une page de détail restaurant -->
-  <Navbar
-    v-if="
-      !excludePaths.includes(route.path) &&
-      user?.role !== 'restaurant' &&
-      user?.is_admin !== true &&
-      route.name !== 'BookingPage' &&
-      !/^\/restaurant\/[0-9]+$/.test(route.path)
-    "
-  />
-
-  <!-- Footer restaurateur : affiché si l'utilisateur est admin -->
-  <RestaurantFooter v-if="showFooter && user?.is_admin === true" />
-  </div>
+    <div class="max-w-[512px] w-full">
+      <router-view />
+      <!-- Navbar pour tous sauf admin -->
+      <Navbar
+        v-if="!user?.is_admin"
+      />
+      <!-- Footer restaurateur : affiché uniquement si l'utilisateur est admin -->
+      <RestaurantFooter v-if="user?.is_admin === true" />
+    </div>
   </div>
 </template>
 
 <script setup>
 import { useRoute } from 'vue-router'
-import { computed } from 'vue'
 import Navbar from './components/NavComposant.vue'
 import RestaurantFooter from './components/RestaurantNav.vue'
 
 const route = useRoute()
 const user = JSON.parse(localStorage.getItem('user') || '{}')
-
-// Routes à exclure (connexion, intro, etc.)
-const excludePaths = [
-  '/intro', '/', '/login', '/register',
-  '/registerdetails', '/loginrestaurant',
-  '/registerrestaurant',
-  '/restaurantdetail',
-  ''
-]
-
-// Masque le footer restaurateur sur les pages exclues et détail restaurant
-const showFooter = computed(() => {
-  if (excludePaths.includes(route.path)) return false
-  if (/^\/restaurant\/[0-9]+$/.test(route.path)) return false
-  return true
-})
 </script>
