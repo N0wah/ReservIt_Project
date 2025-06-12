3ef1b7f02ab2917512e3a89f794c38e1d73b478c
N0wah <noah68mollet@gmail.com>
2025-06-12 10:21:02 +0200

    Update main.js

--- frontend/reservit/src/main.js
+++ frontend/reservit/src/main.js
@@ -14,3 +14,16 @@ app.use(router)
 app.use(VCalendar, {}) 
 
 app.mount('#app')
+
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("Une nouvelle version est disponible. Mettre à jour ?")) {
+      updateSW(true)
+    }
+  },
+  onOfflineReady() {
+    console.log("L'application est prête à fonctionner hors ligne.")
+  }
+})
